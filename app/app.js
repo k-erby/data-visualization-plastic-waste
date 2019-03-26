fffadf15289f7afeb959e8d4b7e6bf446b956c1e
Kaitlin <cmykaitlin@gmail.com>
2019-03-26 00:15:38 -0700

    Add legend and data description placeholders.

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@ Look at the geojson from mapbox and their us-states:
 
 var hoveredCountryId = null;
 
-const displayName ={
+const displayName = {
     0: "Mismanaged Waste",
     1: "Total Waste",
     2: "Kg of Waste Per Person"
@@ -24,28 +24,31 @@ const stylesAndGeoJson = {
                                 "countries-waste.geojson"]
 };
 
+var colors = ['#3e6982', '#709397', '#92b0a4', '#c8d6b8', '#fff4c1', '#e3ba8e', '#b77157', '#b4624d', '#923334'];
 var geoJson = stylesAndGeoJson.percentage_total_waste[1];
 
 
 /* ------------- MapBox API ------------- */
 mapboxgl.accessToken = 'pk.eyJ1IjoiY215a2VyYiIsImEiOiJjanQyNm5jZ28wbHJ1M3lvaHNoZ2pwOGd5In0.4AwOorclWiTE4FmzxkxJOw';
+
 // set the default map view
 var map = new mapboxgl.Map({
     container: 'map',
     style: stylesAndGeoJson.percentage_total_waste[0],
-    center: [20, 42],
+    center: [22, 42],
     zoom: 1
 });
-map.addControl(new mapboxgl.NavigationControl());
 
 refreshMap();
+displayLegend();
+
 
 /* --------- Buttons to switch map view ------- */
-document.getElementById("switch_to_total_waste").onclick = function() {
+document.getElementById("switch_to_total_waste").onclick = () => {
     map = new mapboxgl.Map({
         container: 'map',
         style: stylesAndGeoJson.total_waste[0],
-        center: [20, 42],
+        center: [22, 42],
         zoom: 1
     });
     geoJson = stylesAndGeoJson.total_waste[1];
@@ -53,12 +56,11 @@ document.getElementById("switch_to_total_waste").onclick = function() {
     refreshMap();
 };
 
-
-document.getElementById("switch_to_percent_waste").onclick = function () {
+document.getElementById("switch_to_percent_waste").onclick = () => {
     map = new mapboxgl.Map({
         container: 'map',
         style: stylesAndGeoJson.percentage_total_waste[0],
-        center: [20, 42],
+        center: [22, 42],
         zoom: 1
     });
     geoJson = stylesAndGeoJson.percentage_total_waste[1];
@@ -66,11 +68,11 @@ document.getElementById("switch_to_percent_waste").onclick = function () {
     refreshMap();
 };
 
-document.getElementById("percent_mismanaged_waste").onclick = function () {
+document.getElementById("percent_mismanaged_waste").onclick = () => {
     map = new mapboxgl.Map({
         container: 'map',
         style: stylesAndGeoJson.mismanaged_waste[0],
-        center: [20, 42],
+        center: [22, 42],
         zoom: 1
     });
     geoJson = stylesAndGeoJson.mismanaged_waste[1];
@@ -90,10 +92,7 @@ function refreshMap () {
             "id": "country-borders",
             "type": "fill",
             "source": "countries",
-            'paint': {
-                "fill-color": "#C7E5AF",
-                "fill-opacity": 0
-            }
+            "paint": { "fill-opacity": 0 }
         });
 
         map.addLayer({
@@ -102,40 +101,15 @@ function refreshMap () {
             "source": "countries",
             "layout": {},
             "paint": {
-                "line-color": "#7e9e64",
+                "line-color": "#709397",
                 "line-opacity": 1
             }
         });
     });
 
     // update the map info
-    var mapData = document.getElementById("map-data");
-    mapData.innerHTML = `<h2>${outputMapName()}</h2>`;
-
-
-    /* ------------- Hovering on Countries ------------- */
-    map.on("mouseenter", "country-borders", function (e) {
-        map.getCanvas().style.cursor = 'pointer';
-        hoveredCountryId = e.features[0].properties.name;
-        console.log("ENTER: This is the hovered country: " + hoveredCountryId)
-
-        if (e.features.length > 0) {
-            if (hoveredCountryId) {
-                //map.setFeatureState({source: 'countries', id: hoveredCountryId}, { hover: false});
-            }
-            //map.setFeatureState({source: 'countries', id: hoveredCountryId}, { hover: true});
-        }
-    });
-
-    map.on("mouseleave", "country-borders", function (e) {
-        map.getCanvas().style.cursor = '';
-        console.log("LEAVE: This is the unhovered country: " + hoveredCountryId)
-
-        if (hoveredCountryId) {
-            //map.setFeatureState({source: 'countries', id: hoveredCountryId}, { hover: false});
-        }
-        hoveredCountryId = null;
-    });
+    var mapData = document.getElementById("map-data-title");
+    mapData.innerHTML = `${outputMapName()}`;
 
 
     /* ------------- Clicking Country ------------- */
@@ -143,43 +117,62 @@ function refreshMap () {
         var country = e.features[0].properties.name;
         var value = e.features[0].properties.waste;
 
-
         new mapboxgl.Popup()
             .setLngLat(e.lngLat)
             .setHTML(`<h3>${country}</h3>${outputDataFormat(value)}`)
             .addTo(map);
-
-        console.log("CLICK: This is the clicked country: " + e.features[0].properties.waste)
     });
 }
 
 function outputMapName() {
-
-    if (geoJson === stylesAndGeoJson.mismanaged_waste[1]) {
-        return displayName[0];
-    }
-    else if (geoJson === stylesAndGeoJson.total_waste[1]) {
-        return displayName[1];
-    }
-    else if (geoJson === stylesAndGeoJson.percentage_total_waste[1]) {
-        return displayName[2];
+    switch(geoJson) {
+        case stylesAndGeoJson.mismanaged_waste[1]:          { return displayName[0]; }
+        case stylesAndGeoJson.total_waste[1]:               { return displayName[1]; }
+        case stylesAndGeoJson.percentage_total_waste[1]:    { return displayName[2]; }
     }
 }
 
 function outputDataFormat(value) {
-
-    if (geoJson === stylesAndGeoJson.mismanaged_waste[1]) {
-        return `${value === -1 ? "unknown" : value + "%"}`;
-
+    switch (geoJson) {
+        case stylesAndGeoJson.mismanaged_waste[1]: {
+            return `${value === -1 ? "unknown" : value + "%"}`;
+        }
+        case stylesAndGeoJson.total_waste[1]: {
+            return `${value === -1 ? "unknown" : value + " tonnes/year"}`;
+        }
+        case stylesAndGeoJson.percentage_total_waste[1]: {
+            return `${value === -1 ? "unknown" : value + " kg/person"}`;
+        }
+        default: { return ""; }
     }
-    else if (geoJson === stylesAndGeoJson.total_waste[1]) {
-        return `${value === -1 ? "unknown" : value + " tonnes/year"}`;
+}
+
+function displayLegend() {
+    var legendKeyBox = document.createElement("div");
+    legendKeyBox.className = "legendKeyBox";
+
+    for (i = 0; i < colors.length; i++) {
+      var color = colors[i];
+      var key = document.createElement("span");
+      key.className = "legend-key";
+      key.style.backgroundColor = color;
+
+      legendKeyBox.appendChild(key);
+      legend.appendChild(legendKeyBox);
     }
 
-    else if (geoJson === stylesAndGeoJson.percentage_total_waste[1]) {
-        return `${value === -1 ? "unknown" : value + " kg/person"}`;
-    }
-    return "";
+    var legendText = document.createElement("div");
+    legendText.className = "legendTextBox"
+
+    var legendLess = document.createElement("span");
+    legendLess.innerHTML = "less waste";
+    legendText.appendChild(legendLess);
+
+    var legendMore = document.createElement("span");
+    legendMore.innerHTML = "more waste";
+    legendText.appendChild(legendMore);
+
+    legend.appendChild(legendText);
 }
 
 
