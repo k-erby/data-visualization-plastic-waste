0a4ebfa5f1ddcdc9e0563946195b79ed44311132
Amy Hanvoravongchai <amy.hanv@gmail.com>
2019-04-13 16:41:29 -0700

    Update sidebar design

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 /* ------------- Start sidebar d3 visualization things ------------- */
 
+<<<<<<< HEAD
 	//Initialize radar chart variables
 	var margin = {top: 35, right: 80, bottom: 30, left: 50},
 	width = Math.min(300, window.innerWidth - 10) - margin.left - margin.right,
@@ -71,6 +72,74 @@
 		document.getElementById("switch_to_percent_waste").style.visibility = "visible";
 	};
 	
+=======
+//Initialize radar chart variables
+var margin = { top: 35, right: 80, bottom: 30, left: 50 },
+  width = Math.min(300, window.innerWidth - 10) - margin.left - margin.right,
+  height = Math.min(
+    width,
+    window.innerHeight - margin.top - margin.bottom - 20
+  );
+
+var color = d3.scale.ordinal().range(["#EDC951", "#CC333F"]); //,"#00A0B0"]);
+
+var radarChartOptions = {
+  w: width,
+  h: height,
+  margin: margin,
+  maxValue: 0.5,
+  levels: 5,
+  //labelFactor: 1.2,
+  roundStrokes: true,
+  color: color
+};
+
+var nice_labels = {
+  percent_inadequately_managed: "inadequately managed plastic",
+  percent_plastic_water: "plastic in water streams",
+  percent_plastic: "total plastic"
+};
+
+//note: plastic_waste_complete and averages are imported JSON
+var d3_country_json = JSON.parse(plastic_waste_complete);
+var d3_averages_json = JSON.parse(averages);
+var radar_averages = [];
+for (var key in d3_averages_json) {
+  if (d3_averages_json.hasOwnProperty(key)) {
+    radar_averages.push({
+      axis: nice_labels[key],
+      value: d3_averages_json[key] / 100
+    });
+  }
+}
+
+//Adds current country radar chart to sidebar
+var build_radar = function(country) {
+  var country_json = d3_country_json.filter(function(c) {
+    return c.country == country;
+  })[0];
+
+  console.log(country_json);
+  console.log(d3_averages_json);
+  var radar_country = [];
+  for (var key in country_json) {
+    if (
+      country_json.hasOwnProperty(key) &&
+      (key == "percent_plastic_water" ||
+        key == "percent_inadequately_managed" ||
+        key == "percent_plastic")
+    ) {
+      radar_country.push({
+        axis: nice_labels[key],
+        value: country_json[key] / 100
+      });
+    }
+  }
+
+  //show chart
+  RadarChart(".radarChart", [radar_averages, radar_country], radarChartOptions);
+};
+>>>>>>> Update sidebar design
 /* ------------- End sidebar d3 ------------- */
 
 /* ----------------------------------------------------
@@ -84,12 +153,19 @@ Look at the geojson from mapbox and their us-states:
 var hoveredCountryId = null;
 
 const displayName = {
-    0: "Mismanaged Waste",
-    1: "Total Waste",
-    2: "Kg of Waste Per Person"
+  0: "Mismanaged Waste",
+  1: "Total Waste",
+  2: "Kg of Waste Per Person"
+};
+
+const displayIcon = {
+  0: "img/trash.png",
+  1: "img/trash.png",
+  2: "img/person.png"
 };
 
 const dataDescriptions = {
+<<<<<<< HEAD
     0: "Plastic waste that is inadequately disposed of as a percentage of the country's total waste generation."
         + "<br><br>This includes disposal in dumps or open, uncontrolled landfills, where it is not fully contained"
         + " and has a high risk of polluting rivers and oceans. This does not include 'littered'"
@@ -101,102 +177,175 @@ const dataDescriptions = {
         + " overall per capita plastic waste generation rate prior to waste management, recycling or incineration."
         + " It does not therefore directly indicate the risk of pollution to waterways or marine environments."
 }
+=======
+  0:
+    "Plastic waste that is inadequately disposed of as a percentage of the country's total waste generation." +
+    "<br><br>This includes disposal in dumps or open, uncontrolled landfills, where it is not fully contained" +
+    " and has a high risk of polluting rivers and oceans. This does not include 'littered'" +
+    " plastic waste (approximately 2% of total waste).",
+  1:
+    "Total plastic waste generation by country, measured in tonnes per year, prior to waste management," +
+    " recycling, or incineration.<br><br>High-income countries typically have well-managed" +
+    " waste streams and therefore low levels of plastic pollution to external environments.",
+  2:
+    "Daily plastic waste generation per person, measured in kilograms per person per day.<br><br>This measures the" +
+    " overall per capita plastic waste generation rate prior to waste management, recycling or incineration." +
+    " It does not therefore directly indicate the risk of pollution to waterways or marine environments."
+  /*0: "Share of total plastic waste that is inadequately managed. Inadequately disposed waste is not formally managed"
+        + " and includes disposal in dumps or open, uncontrolled landfills, where it is not fully contained."
+        + " Inadequately managed waste has high risk of polluting rivers and oceans. This does not include 'littered'"
+        + " plastic waste, which is approximately 2% of total waste (including high-income countries).",
+    1: "Total plastic waste generation by country, measured in tonnes per year. This measures total plastic waste"
+        + " generation prior to management and therefore does not represent the quantity of plastic at risk of"
+        + " polluting waterways, rivers and the ocean environment. High-income countries typically have well-managed"
+        + " waste streams and therefore low levels of plastic pollution to external environments.",
+    2: "Daily plastic waste generation per person, measured in kilograms per person per day. This measures the"
+        + " overall per capita plastic waste generation rate prior to waste management, recycling or incineration."
+        + " It does not therefore directly indicate the risk of pollution to waterways or marine environments."*/
+};
+>>>>>>> Update sidebar design
 
 const stylesAndGeoJson = {
-    mismanaged_waste: ["mapbox://styles/cmykerb/cjtnfon9y2th11fph12fe3k9j",
-                        "countries-mismanaged-waste-total.geojson"],
-    total_waste: ["mapbox://styles/cmykerb/cjtnghjlm24kv1fmrutxx082z",
-                    "countries-plastic-waste-total.geojson"],
-    percentage_total_waste: ["mapbox://styles/cmykerb/cjtkowpvw19ji1fs2549azkn9",
-                                "countries-waste.geojson"]
+  mismanaged_waste: [
+    "mapbox://styles/cmykerb/cjtnfon9y2th11fph12fe3k9j",
+    "countries-mismanaged-waste-total.geojson"
+  ],
+  total_waste: [
+    "mapbox://styles/cmykerb/cjtnghjlm24kv1fmrutxx082z",
+    "countries-plastic-waste-total.geojson"
+  ],
+  percentage_total_waste: [
+    "mapbox://styles/cmykerb/cjtkowpvw19ji1fs2549azkn9",
+    "countries-waste.geojson"
+  ]
 };
 
-var colors = ['#3e6982', '#709397', '#92b0a4', '#c8d6b8', '#fff4c1', '#e3ba8e', '#b77157', '#b4624d', '#923334'];
+var colors = [
+  "#3e6982",
+  "#709397",
+  "#92b0a4",
+  "#c8d6b8",
+  "#fff4c1",
+  "#e3ba8e",
+  "#b77157",
+  "#b4624d",
+  "#923334"
+];
 var geoJson = stylesAndGeoJson.percentage_total_waste[1];
 
-
 /* ------------- MapBox API ------------- */
-mapboxgl.accessToken = 'pk.eyJ1IjoiY215a2VyYiIsImEiOiJjanQyNm5jZ28wbHJ1M3lvaHNoZ2pwOGd5In0.4AwOorclWiTE4FmzxkxJOw';
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiY215a2VyYiIsImEiOiJjanQyNm5jZ28wbHJ1M3lvaHNoZ2pwOGd5In0.4AwOorclWiTE4FmzxkxJOw";
 
 // set the default map view
 var map = new mapboxgl.Map({
-    container: 'map',
-    style: stylesAndGeoJson.percentage_total_waste[0],
-    center: [22, 42],
-    zoom: 1
+  container: "map",
+  style: stylesAndGeoJson.percentage_total_waste[0],
+  center: [22, 42],
+  zoom: 1
 });
 
 refreshMap();
 displayLegend();
 
-
 /* --------- Buttons to switch map view ------- */
 document.getElementById("switch_to_total_waste").onclick = () => {
-    map = new mapboxgl.Map({
-        container: 'map',
-        style: stylesAndGeoJson.total_waste[0],
-        center: [22, 42],
-        zoom: 1
-    });
-    geoJson = stylesAndGeoJson.total_waste[1];
+  map = new mapboxgl.Map({
+    container: "map",
+    style: stylesAndGeoJson.total_waste[0],
+    center: [22, 42],
+    zoom: 1
+  });
+  geoJson = stylesAndGeoJson.total_waste[1];
 
-    refreshMap();
+  refreshMap();
 };
 
 document.getElementById("switch_to_percent_waste").onclick = () => {
-    map = new mapboxgl.Map({
-        container: 'map',
-        style: stylesAndGeoJson.percentage_total_waste[0],
-        center: [22, 42],
-        zoom: 1
-    });
-    geoJson = stylesAndGeoJson.percentage_total_waste[1];
+  map = new mapboxgl.Map({
+    container: "map",
+    style: stylesAndGeoJson.percentage_total_waste[0],
+    center: [22, 42],
+    zoom: 1
+  });
+  geoJson = stylesAndGeoJson.percentage_total_waste[1];
 
-    refreshMap();
+  refreshMap();
 };
 
 document.getElementById("percent_mismanaged_waste").onclick = () => {
-    map = new mapboxgl.Map({
-        container: 'map',
-        style: stylesAndGeoJson.mismanaged_waste[0],
-        center: [22, 42],
-        zoom: 1
-    });
-    geoJson = stylesAndGeoJson.mismanaged_waste[1];
+  map = new mapboxgl.Map({
+    container: "map",
+    style: stylesAndGeoJson.mismanaged_waste[0],
+    center: [22, 42],
+    zoom: 1
+  });
+  geoJson = stylesAndGeoJson.mismanaged_waste[1];
 
-    refreshMap();
+  refreshMap();
 };
 
-
-function refreshMap () {
-    map.on('load', function () {
-        map.addSource("countries", {
-            "type": "geojson",
-            "data": "../json/" + geoJson
-        });
-
-        map.addLayer({
-            "id": "country-borders",
-            "type": "fill",
-            "source": "countries",
-            "paint": { "fill-opacity": 0 }
-        });
-
-        map.addLayer({
-            "id": "country-lines",
-            "type": "line",
-            "source": "countries",
-            "layout": {},
-            "paint": {
-                "line-color": "#709397",
-                "line-opacity": 1
-            }
-        });
+function refreshMap() {
+  map.on("load", function() {
+    map.addSource("countries", {
+      type: "geojson",
+      data: "../json/" + geoJson
     });
 
-    // update the map info
+    map.addLayer({
+      id: "country-borders",
+      type: "fill",
+      source: "countries",
+      paint: { "fill-opacity": 0 }
+    });
+
+    map.addLayer({
+      id: "country-lines",
+      type: "line",
+      source: "countries",
+      layout: {},
+      paint: {
+        "line-color": "#709397",
+        "line-opacity": 1
+      }
+    });
+  });
+
+  // update the map info
+  var mapData = document.getElementById("map-data-title");
+  var mapDataDescription = document.getElementById("map-data-description");
+  var mapIcon = document.getElementById("map-icon");
+  mapData.innerHTML = `${outputMapName()}`;
+  mapDataDescription.innerHTML = `${outputDataDescription()}`;
+  mapIcon.src = `${outputIcon()}`;
+
+  /* ------------- Country Specific Actions ------------- */
+
+  var popup = new mapboxgl.Popup();
+
+  //Hover pop-up
+  map.on("mouseenter", "country-borders", function(e) {
+    map.getCanvas().style.cursor = "pointer";
+    var country = e.features[0].properties.name;
+    var value = e.features[0].properties.waste;
+
+    popup
+      .setLngLat(e.lngLat)
+      .setHTML(`<h3>${country}</h3>${outputDataFormat(value)}`)
+      .addTo(map);
+  });
+
+  //Click to modify sidebar
+  map.on("click", "country-borders", function(e) {
+    var country = e.features[0].properties.name;
+    console.log(country);
+
+    build_radar(country);
+
+    //Modify sidebar labels
     var mapData = document.getElementById("map-data-title");
     var mapDataDescription = document.getElementById("map-data-description");
+<<<<<<< HEAD
     mapData.innerHTML = `${outputMapName()}`;
     mapDataDescription.innerHTML = `${outputDataDescription()}`;
 
@@ -251,65 +400,125 @@ function refreshMap () {
 		map.getCanvas().style.cursor = '';
 		popup.remove();
 	});
+=======
+    mapData.innerHTML = `${country}`;
+    mapDataDescription.innerHTML = `The radar chart shows how ${country} (red) differs from average plastic waste impact (yellow).<br><br>Percentages are in terms of total country waste.`;
+
+    //Hide nav buttons
+    var buttons = document.getElementsByClassName("map-button-list")[0];
+    buttons.style.visibility = "hidden";
+
+    //Add exit button
+    /*var sidebar = document.getElementsByClassName("map-info")[0];
+		var exit_button = document.createElement("button");
+		exit_button.type = "button";
+		exit_button.id = "exit_button";
+		exit_button.innerHTML = "X";
+		sidebar.prepend(exit_button);
+		*/
+    //exit_button.onclick = func;
+
+    //TODO: add radar legend
+  });
+
+  //TODO: add removal of radarChart
+  //on-click of exit button
+
+  //TODO: display total waste (current, average)
+
+  //Clear hover pop-up
+  map.on("mouseleave", "country-borders", function() {
+    map.getCanvas().style.cursor = "";
+    popup.remove();
+  });
+>>>>>>> Update sidebar design
 }
 
 function outputMapName() {
-    switch(geoJson) {
-        case stylesAndGeoJson.mismanaged_waste[1]:          { return displayName[0]; }
-        case stylesAndGeoJson.total_waste[1]:               { return displayName[1]; }
-        case stylesAndGeoJson.percentage_total_waste[1]:    { return displayName[2]; }
+  switch (geoJson) {
+    case stylesAndGeoJson.mismanaged_waste[1]: {
+      return displayName[0];
     }
+    case stylesAndGeoJson.total_waste[1]: {
+      return displayName[1];
+    }
+    case stylesAndGeoJson.percentage_total_waste[1]: {
+      return displayName[2];
+    }
+  }
 }
 
-function outputDataDescription() {
-    switch(geoJson) {
-        case stylesAndGeoJson.mismanaged_waste[1]:          { return dataDescriptions[0]; }
-        case stylesAndGeoJson.total_waste[1]:               { return dataDescriptions[1]; }
-        case stylesAndGeoJson.percentage_total_waste[1]:    { return dataDescriptions[2]; }
+function outputIcon() {
+  switch (geoJson) {
+    case stylesAndGeoJson.mismanaged_waste[1]: {
+      return displayIcon[0];
     }
+    case stylesAndGeoJson.total_waste[1]: {
+      return displayIcon[1];
+    }
+    case stylesAndGeoJson.percentage_total_waste[1]: {
+      return displayIcon[2];
+    }
+  }
+}
+function outputDataDescription() {
+  switch (geoJson) {
+    case stylesAndGeoJson.mismanaged_waste[1]: {
+      return dataDescriptions[0];
+    }
+    case stylesAndGeoJson.total_waste[1]: {
+      return dataDescriptions[1];
+    }
+    case stylesAndGeoJson.percentage_total_waste[1]: {
+      return dataDescriptions[2];
+    }
+  }
 }
 
 function outputDataFormat(value) {
-    switch (geoJson) {
-        case stylesAndGeoJson.mismanaged_waste[1]: {
-            return `${value === -1 ? "unknown" : value + "%"}`;
-        }
-        case stylesAndGeoJson.total_waste[1]: {
-            return value == -1 ? value = "unknown" : value.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') + " tonnes/year";
-        }
-        case stylesAndGeoJson.percentage_total_waste[1]: {
-            return `${value === -1 ? "unknown" : value + " kg/person"}`;
-        }
-        default: { return ""; }
+  switch (geoJson) {
+    case stylesAndGeoJson.mismanaged_waste[1]: {
+      return `${value === -1 ? "unknown" : value + "%"}`;
     }
+    case stylesAndGeoJson.total_waste[1]: {
+      return value == -1
+        ? (value = "unknown")
+        : value.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,") +
+            " tonnes/year";
+    }
+    case stylesAndGeoJson.percentage_total_waste[1]: {
+      return `${value === -1 ? "unknown" : value + " kg/person"}`;
+    }
+    default: {
+      return "";
+    }
+  }
 }
 
 function displayLegend() {
-    var legendKeyBox = document.createElement("div");
-    legendKeyBox.className = "legendKeyBox";
+  var legendKeyBox = document.createElement("div");
+  legendKeyBox.className = "legendKeyBox";
 
-    for (i = 0; i < colors.length; i++) {
-      var color = colors[i];
-      var key = document.createElement("span");
-      key.className = "legend-key";
-      key.style.backgroundColor = color;
+  for (i = 0; i < colors.length; i++) {
+    var color = colors[i];
+    var key = document.createElement("span");
+    key.className = "legend-key";
+    key.style.backgroundColor = color;
 
-      legendKeyBox.appendChild(key);
-      legend.appendChild(legendKeyBox);
-    }
+    legendKeyBox.appendChild(key);
+    legend.appendChild(legendKeyBox);
+  }
 
-    var legendText = document.createElement("div");
-    legendText.className = "legendTextBox"
+  var legendText = document.createElement("div");
+  legendText.className = "legendTextBox";
 
-    var legendLess = document.createElement("span");
-    legendLess.innerHTML = "less waste";
-    legendText.appendChild(legendLess);
+  var legendLess = document.createElement("span");
+  legendLess.innerHTML = "less waste";
+  legendText.appendChild(legendLess);
 
-    var legendMore = document.createElement("span");
-    legendMore.innerHTML = "more waste";
-    legendText.appendChild(legendMore);
+  var legendMore = document.createElement("span");
+  legendMore.innerHTML = "more waste";
+  legendText.appendChild(legendMore);
 
-    legend.appendChild(legendText);
+  legend.appendChild(legendText);
 }
-
-
