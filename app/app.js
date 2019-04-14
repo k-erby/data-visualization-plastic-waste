52812e8e807fb41439890f81931b2b38accce071
Amy Hanvoravongchai <amy.hanv@gmail.com>
2019-04-13 17:23:33 -0700

    Change icon colors

--- app/app.js
+++ app/app.js
@@ -1,78 +1,5 @@
 /* ------------- Start sidebar d3 visualization things ------------- */
 
-<<<<<<< HEAD
-	//Initialize radar chart variables
-	var margin = {top: 35, right: 80, bottom: 30, left: 50},
-	width = Math.min(300, window.innerWidth - 10) - margin.left - margin.right,
-	height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20);
-	
-	var color = d3.scale.ordinal()
-				.range(["#ffe880", "#923334"]);//["#EDC951","#CC333F","#00A0B0"]);
-	
-	var radarChartOptions = {
-	  w: width,
-	  h: height,
-	  margin: margin,
-	  maxValue: 0.5,
-	  levels: 5,
-	  //labelFactor: 1.2,
-	  roundStrokes: true,
-	  color: color
-	};
-	
-	var nice_labels = {"percent_inadequately_managed":"inadequately managed plastic", "percent_plastic_water":"plastic in water streams", "percent_plastic":"total plastic"};
-	
-	//note: plastic_waste_complete and averages are imported JSON
-	var d3_country_json = JSON.parse(plastic_waste_complete);
-	var d3_averages_json = JSON.parse(averages);
-	var radar_averages = [];
-	for (var key in d3_averages_json){
-		if (d3_averages_json.hasOwnProperty(key)) {
-			radar_averages.push({"axis": nice_labels[key], "value": d3_averages_json[key]/100});
-		}
-	}
-	
-	//Adds current country radar chart to sidebar
-	var build_radar = function(country){
-	var country_json = d3_country_json.filter(function(c){return c.country == country})[0];
-  
-		console.log(country_json);
-		console.log(d3_averages_json);
-		var radar_country = [];
-		for (var key in country_json){
-			if (country_json.hasOwnProperty(key) && (key == "percent_plastic_water" || key == "percent_inadequately_managed" || key == "percent_plastic")){
-				radar_country.push({"axis": nice_labels[key], "value": country_json[key]/100});
-			}
-		}
-
-		//show chart
-		RadarChart(".radarChart", [radar_averages,radar_country], radarChartOptions);
-	};
-	
-	function remove_sidebar(title){
-		//Remove RadarChart and exit button
-		document.getElementsByTagName("svg")[0].remove();
-		document.getElementById("exit_button").remove();
-		
-		//Return sidebar to original view
-		if(title == displayName[0]){
-			document.getElementById("percent_mismanaged_waste").click();
-			console.log("case 1");
-		} else if (title == displayName[1]){
-			document.getElementById("switch_to_total_waste").click();
-			console.log("case 2");
-		} else {
-			document.getElementById("switch_to_percent_waste").click();
-			console.log("case 3");
-		}
-		
-		//Un-hide nav buttons
-		document.getElementById("percent_mismanaged_waste").style.visibility = "visible";
-		document.getElementById("switch_to_total_waste").style.visibility = "visible";
-		document.getElementById("switch_to_percent_waste").style.visibility = "visible";
-	};
-	
-=======
 //Initialize radar chart variables
 var margin = { top: 35, right: 80, bottom: 30, left: 50 },
   width = Math.min(300, window.innerWidth - 10) - margin.left - margin.right,
@@ -139,7 +66,6 @@ var build_radar = function(country) {
   //show chart
   RadarChart(".radarChart", [radar_averages, radar_country], radarChartOptions);
 };
->>>>>>> Update sidebar design
 /* ------------- End sidebar d3 ------------- */
 
 /* ----------------------------------------------------
@@ -159,13 +85,12 @@ const displayName = {
 };
 
 const displayIcon = {
-  0: "img/trash.png",
+  0: "img/banana.png",
   1: "img/trash.png",
   2: "img/person.png"
 };
 
 const dataDescriptions = {
-<<<<<<< HEAD
     0: "Plastic waste that is inadequately disposed of as a percentage of the country's total waste generation."
         + "<br><br>This includes disposal in dumps or open, uncontrolled landfills, where it is not fully contained"
         + " and has a high risk of polluting rivers and oceans. This does not include 'littered'"
@@ -176,34 +101,7 @@ const dataDescriptions = {
     2: "Daily plastic waste generation per person, measured in kilograms per person per day.<br><br>This measures the"
         + " overall per capita plastic waste generation rate prior to waste management, recycling or incineration."
         + " It does not therefore directly indicate the risk of pollution to waterways or marine environments."
-}
-=======
-  0:
-    "Plastic waste that is inadequately disposed of as a percentage of the country's total waste generation." +
-    "<br><br>This includes disposal in dumps or open, uncontrolled landfills, where it is not fully contained" +
-    " and has a high risk of polluting rivers and oceans. This does not include 'littered'" +
-    " plastic waste (approximately 2% of total waste).",
-  1:
-    "Total plastic waste generation by country, measured in tonnes per year, prior to waste management," +
-    " recycling, or incineration.<br><br>High-income countries typically have well-managed" +
-    " waste streams and therefore low levels of plastic pollution to external environments.",
-  2:
-    "Daily plastic waste generation per person, measured in kilograms per person per day.<br><br>This measures the" +
-    " overall per capita plastic waste generation rate prior to waste management, recycling or incineration." +
-    " It does not therefore directly indicate the risk of pollution to waterways or marine environments."
-  /*0: "Share of total plastic waste that is inadequately managed. Inadequately disposed waste is not formally managed"
-        + " and includes disposal in dumps or open, uncontrolled landfills, where it is not fully contained."
-        + " Inadequately managed waste has high risk of polluting rivers and oceans. This does not include 'littered'"
-        + " plastic waste, which is approximately 2% of total waste (including high-income countries).",
-    1: "Total plastic waste generation by country, measured in tonnes per year. This measures total plastic waste"
-        + " generation prior to management and therefore does not represent the quantity of plastic at risk of"
-        + " polluting waterways, rivers and the ocean environment. High-income countries typically have well-managed"
-        + " waste streams and therefore low levels of plastic pollution to external environments.",
-    2: "Daily plastic waste generation per person, measured in kilograms per person per day. This measures the"
-        + " overall per capita plastic waste generation rate prior to waste management, recycling or incineration."
-        + " It does not therefore directly indicate the risk of pollution to waterways or marine environments."*/
 };
->>>>>>> Update sidebar design
 
 const stylesAndGeoJson = {
   mismanaged_waste: [
@@ -345,62 +243,7 @@ function refreshMap() {
     //Modify sidebar labels
     var mapData = document.getElementById("map-data-title");
     var mapDataDescription = document.getElementById("map-data-description");
-<<<<<<< HEAD
-    mapData.innerHTML = `${outputMapName()}`;
-    mapDataDescription.innerHTML = `${outputDataDescription()}`;
 
-    /* ------------- Country Specific Actions ------------- */
-	
-	var popup = new mapboxgl.Popup()
-	
-	//Hover pop-up
-    map.on('mouseenter', 'country-borders', function (e) {
-		map.getCanvas().style.cursor = 'pointer';
-        var country = e.features[0].properties.name;
-        var value = e.features[0].properties.waste;
-
-		popup.setLngLat(e.lngLat)
-            .setHTML(`<h3>${country}</h3>${outputDataFormat(value)}`)
-            .addTo(map);
-    });
-	
-	//Click to modify sidebar
-	map.on('click', 'country-borders', function (e) {
-        var country = e.features[0].properties.name;
-		console.log(country);
-			
-		build_radar(country);
-		   
-		//Modify sidebar labels
-		var mapData = document.getElementById("map-data-title");
-		var mapDataDescription = document.getElementById("map-data-description");
-		var oldTitle = mapData.textContent;
-		mapData.innerHTML = `${country}`;
-		mapDataDescription.innerHTML = `The radar chart shows how ${country} (red) differs from average plastic waste impact (yellow).<br><br>Percentages are in terms of total country waste.`;
-		
-		//Hide nav buttons
-		document.getElementById("percent_mismanaged_waste").style.visibility = "hidden";
-		document.getElementById("switch_to_total_waste").style.visibility = "hidden";
-		document.getElementById("switch_to_percent_waste").style.visibility = "hidden";
-		
-		if (!document.contains(document.getElementById("exit_button"))) {
-			//Add exit button
-			var exit_button = document.createElement("button");
-			exit_button.type = "button";
-			exit_button.innerHTML = "Exit";
-			exit_button.id = "exit_button";
-			exit_button.className = "map-button";
-			document.getElementsByClassName("map-button-list")[0].prepend(exit_button);
-		}
-		document.getElementById("exit_button").onclick = () => remove_sidebar(oldTitle);
-    });
-	
-	//Clear hover pop-up
-	map.on('mouseleave', 'country-borders', function() {
-		map.getCanvas().style.cursor = '';
-		popup.remove();
-	});
-=======
     mapData.innerHTML = `${country}`;
     mapDataDescription.innerHTML = `The radar chart shows how ${country} (red) differs from average plastic waste impact (yellow).<br><br>Percentages are in terms of total country waste.`;
 
@@ -431,7 +274,6 @@ function refreshMap() {
     map.getCanvas().style.cursor = "";
     popup.remove();
   });
->>>>>>> Update sidebar design
 }
 
 function outputMapName() {
